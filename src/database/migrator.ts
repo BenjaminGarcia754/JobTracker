2c660a1300c7ea1c25d8bf319021e3600721bd10
Benjamín del Ángel García Hernández <linkdelira@gmail.com>
2026-09-22 13:01:06 -0600

    Refactor migrator.ts to search migrations automatically

--- src/database/migrator.ts
+++ src/database/migrator.ts
@@ -1,15 +1,46 @@
 import type { Sequelize } from 'sequelize';
 import { Umzug, SequelizeStorage } from 'umzug';
-
-import * as applications from './migrations/001-create-applications.js';
+import path from 'node:path';
+import { pathToFileURL } from 'node:url';
 
 export function createMigrator(database: Sequelize) {
   return new Umzug({
-    migrations: [
-      { name: '001-create-applications', ...applications }
-    ],
+    migrations: {
+      glob: ['migrations/**/*.{ts,js}', {
+        cwd: import.meta.dirname,
+      }],
+
+      resolve: ({ name, path: migrationPath, context }) => {
+        const normalizedName = path.parse(name).name;
+
+        return {
+          name: normalizedName,
+
+          up: async () => {
+            const migration = await import(
+              pathToFileURL(migrationPath!).href
+            );
+
+            await migration.up({ context });
+          },
+
+          down: async () => {
+            const migration = await import(
+              pathToFileURL(migrationPath!).href
+            );
+
+            await migration.down({ context });
+          },
+        };
+      },
+    },
+
     context: database.getQueryInterface(),
-    storage: new SequelizeStorage({ sequelize: database }),
+
+    storage: new SequelizeStorage({
+      sequelize: database,
+    }),
+
     logger: console,
   });
 }
